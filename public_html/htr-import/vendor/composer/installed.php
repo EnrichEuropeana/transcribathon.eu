c5487ebdf213ccedf39b5b96d5ed305bf6be9538
trenc <github@tommyschmucker.de>
2022-08-31 13:46:48 +0200

    fix: cleanup obsolete files, add vendor libs

---
+++ public_html/htr-import/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '0.1.0',
+    'version' => '0.1.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'factsandfiles/transcribus-api-client',
+  ),
+  'versions' => 
+  array (
+    'factsandfiles/transcribus-api-client' => 
+    array (
+      'pretty_version' => '0.1.0',
+      'version' => '0.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'kint-php/kint' => 
+    array (
+      'pretty_version' => '3.3',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '335ac1bcaf04d87df70d8aa51e8887ba2c6d203b',
+    ),
+  ),
+);
