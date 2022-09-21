beb844b99fced7f04fd7e6f5d37986edd243dd32
trenc <github@tommyschmucker.de>
2022-09-21 15:57:48 +0200

    refactor: also check php input stream

--- public_html/wp-content/themes/transcribathon/admin/inc/custom_scripts/send_ajax_api_request.php
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_scripts/send_ajax_api_request.php
@@ -1,39 +1,42 @@
-<?php 
-//include(str_repeat("../",(sizeof(explode("/",substr((string)getcwd(),strrpos((string)getcwd(),"/wp-content"),strlen((string)getcwd()))))-2))."../wp-load.php");
+<?php
+require_once( $_SERVER["DOCUMENT_ROOT"].'/wp-load.php' );
 require_once( $_SERVER["DOCUMENT_ROOT"].'/wp-admin/includes/post.php' );
 
+$post = file_get_contents('php://input');
+$post = $post ? json_decode($post, true) : $_POST;
 
-if(isset($_POST['type']) && isset($_POST['url'])){
+if (!empty($post['type']) && !empty($post['url'])) {
+
+
     // Set Post content
-    $data = array(
-    );
-    if (isset($_POST['data']) && $_POST['data'] != null) {
-        foreach ($_POST['data'] as $key => $value) {
+    $data = array();
+    if (isset($post['data']) && $post['data'] != null) {
+        foreach ($post['data'] as $key => $value) {
             $data[$key] = $value;
         }
     }
     $postContent = json_encode($data);
-    
+
     // Prepare new cURL resource
-    $ch = curl_init($_POST['url']);
+    $ch = curl_init($post['url']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_POST['type']);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $post['type']);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postContent);
-    
-    // Set HTTP Header for request 
+
+    // Set HTTP Header for request
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
         'Content-Length: ' . strlen($postContent))
     );
-    
+
     // Submit the request
     $result = curl_exec($ch);
 
     // Get response code
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    
-    // Close cURL session handle 
+
+    // Close cURL session handle
     curl_close($ch);
 
     // return response
