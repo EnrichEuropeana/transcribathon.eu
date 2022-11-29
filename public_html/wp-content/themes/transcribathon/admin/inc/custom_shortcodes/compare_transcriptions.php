d92122107e144151df819ac4a619c0b51cb7585e
trenc <github@tommyschmucker.de>
2022-11-29 12:00:02 +0100

    fix: remove script from other side

--- public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/compare_transcriptions.php
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/compare_transcriptions.php
@@ -532,17 +532,16 @@ function _TCT_compare_transcriptions( $atts) {
     $content .= "<section id='viewer-n-transcription' class='collapsed'>";
         $content .= "<div id='full-view-container'>";
             // Mark as active checkboxes
-            $content .= "<div id='mark-active'>";
-                $content .= "<div id='htr-check' style='float:left;'>";
-                    $content .= "<input type='checkbox' id='htr-box' name='htr-box'>";
-                    $content .= "<label for='htr-box' style='margin-left: 10px;'>Mark HTR Transcription as active</label>";
-                $content .= "</div>";
-                $content .= "<div id='man-check' style='float:right;'>";
-                    $content .= "<input type='checkbox' id='man-box' name='man-box'>";
-                    $content .= "<label for='man-box' style='margin-left: 10px;'>Mark Manual Transcription as active</label>";
-                $content .= "</div>";
+            $content .= "<div class='mark-active'>";
+                $content .= "<label>";
+                    $content .= "<input type='radio' name='mark_active' data-action='mark_active'>";
+                    $content .= "Mark HTR Transcription as active";
+                $content .= "</label>";
+                    $content .= "<label>";
+                        $content .= "<input type='radio' name='mark_active' data-action='mark_active'>";
+                        $content .= "Mark Manual Transcription as active";
+                    $content .= "</label>";
             $content .= "</div>";
-            $content .= "<div style='clear:both;'></div>";
         //$content .= "<section id='viewer-n-transcription'>";
             $content .= "<div id='full-view-l'>";
                 $content .= "<div id='htr-container' style='height:600px;'>";
@@ -736,203 +735,11 @@ function _TCT_compare_transcriptions( $atts) {
 
 
         $content .= "</div>"; // end of full-view-container
-    // JAVASCRIPT TODO put in a separate file
-    $content .= "<script>
-
-    var ready = (callback) => {
-        if (document.readyState != \"loading\") callback();
-        else document.addEventListener(\"DOMContentLoaded\", callback);
-    }
-    // Replacement for jQuery document.ready; It runs the code after DOM is completely loaded
-    ready(() => {
-        const collapseBtn = document.querySelector('#transcription-collapse-btn');
-        const trSection = document.querySelector('#viewer-n-transcription');
-        // Transcription Collapse
-        if(collapseBtn) {
-            collapseBtn.addEventListener('click', function() {
-                if(trSection.classList.contains('collapsed')){
-                    trSection.classList.remove('collapsed');
-                    document.querySelector('#transcription-container').style.height = 'unset';
-                    document.querySelector('#htr-container').style.height = 'unset';
-                    collapseBtn.textContent = 'Show Less';
-                } else {
-                    trSection.classList.add('collapsed');
-                    trSection.style.height = '600px';
-                    document.querySelector('#transcription-container').style.height = '600px';
-                    document.querySelector('#htr-container').style.height = '600px';
-                    collapseBtn.textContent = 'Show More';
-                }
-            });
-        }
-        // New Js for Image slider
-        const imgSliderCheck = document.querySelector('#img-slider');
-        if(imgSliderCheck) {
-            // function to show/hide images
-            function showImages(start, end, images) {
-                for(let img of images) {
-                    if(img.getAttribute('data-value') < start || img.getAttribute('data-value') > end) {
-                        img.style.display = 'none';
-                    } else {
-                        img.style.display = 'inline-block';
-                    }
-                }
-            }
-            // Only item page(start slider with the item on the page)
-            const currentItem = document.querySelector('#slide-start');
-            //
-            const imgStickers = document.querySelectorAll('.slide-sticker');
-            const windowWidth = document.querySelector('#img-slider').clientWidth;
-            let sliderStart = 1; // First Image to the left
-            let sliderEnd = 0; // Last Image to the right
-            const nextSet = document.querySelector('.next-slide');
-            const prevSet = document.querySelector('.prev-slide');
-            const leftSpanNumb = document.querySelector('#left-num');
-            const rightSpanNumb = document.querySelector('#right-num');
-            let currentDot = 1;
-            let step = 0; // number of images on screen
-            if(windowWidth > 1200) {
-                step = 9;
-            } else if(windowWidth > 800) {
-                step = 5;
-            } else {
-                step = 3;
-            }
-
-            sliderEnd = step;
-
-            if(imgStickers.length <= step){
-                prevSet.style.display = 'none';
-                nextSet.style.display = 'none';
-            }
-            leftSpanNumb.textContent = sliderStart;
-            rightSpanNumb.textContent = sliderEnd;
-            // check if there are more images than it fits on the screen
-            if(nextSet.style.display != 'none') {
-                showImages(sliderStart, sliderEnd, imgStickers);
-            }
-            // Slider dots
-            const dotContainer = document.querySelector('#dot-indicators');
-            const numberDots = Math.ceil(imgStickers.length / step);
-            for(let i = 0; i < numberDots; i++) {
-                const sliderDot = document.createElement('div');
-                sliderDot.classList.add('slider-dot');
-                sliderDot.setAttribute('data-value', (i+1));
-                dotContainer.appendChild(sliderDot);
-            }
-
-            const sliderDots = document.querySelectorAll('.slider-dot');
-
-            for(let dot of sliderDots) {
-                dot.addEventListener('click', function() {
-                    currentDot = parseInt(dot.getAttribute('data-value'));
-                    dot.classList.add('current');
-                    if(dot.getAttribute('data-value') * step > imgStickers.length) {
-                        sliderStart = (imgStickers.length - step) + 1;
-                        sliderEnd = imgStickers.length;
-                    } else {
-                        sliderEnd = parseInt(dot.getAttribute('data-value')) * step;
-                        sliderStart = (sliderEnd - step) + 1;
-                    }
-                    showImages(sliderStart, sliderEnd, imgStickers);
-                    leftSpanNumb.textContent = sliderStart;
-                    rightSpanNumb.textContent = sliderEnd;
-                    for(let dot of sliderDots) {
-                        if(dot.getAttribute('data-value') < currentDot || dot.getAttribute('data-value') > currentDot) {
-                            if(dot.classList.contains('current')){
-                                dot.classList.remove('current');
-                            }
-                        }
-                    }
-                })
-            }
-            if(currentItem) {
-              let currentPosition = Math.floor(parseInt(currentItem.textContent) / step);
-              for(let dot of sliderDots) {
-                if(currentPosition + 1 === parseInt(dot.getAttribute('Data-value'))){
-                  dot.click();
-                }
-              }
-            }
-            nextSet.addEventListener('click', function() {
-                currentDot += 1;
-                if(currentDot > numberDots ) {
-                    currentDot = 1;
-                }
-                if(rightSpanNumb.textContent == imgStickers.length) {
-                    sliderStart = 1;
-                    sliderEnd = step;
-                } else if(sliderEnd + step <= imgStickers.length) {
-                    sliderStart = sliderStart + step;
-                    sliderEnd = sliderEnd + step;
-                } else {
-                    sliderStart = (imgStickers.length - step) + 1;
-                    sliderEnd = imgStickers.length;
-                }
-                showImages(sliderStart, sliderEnd, imgStickers);
-                leftSpanNumb.textContent = sliderStart;
-                rightSpanNumb.textContent = sliderEnd;
-                for(let dot of sliderDots) {
-                    if(parseInt(dot.getAttribute('data-value')) < currentDot || parseInt(dot.getAttribute('data-value')) > currentDot) {
-                        if(dot.classList.contains('current')){
-                            dot.classList.remove('current');
-                        }
-                    } else {
-                        dot.classList.add('current');
-                    }
-                }
-            })
-            prevSet.addEventListener('click', function() {
-                if(currentDot - 1 < 1) {
-                    currentDot = numberDots;
-                } else {
-                    currentDot -= 1;
-                }
-                if(leftSpanNumb.textContent == '1') {
-                    sliderEnd = imgStickers.length;
-                    sliderStart = (imgStickers.length - step) + 1;
-                } else if(sliderStart - step < 1) {
-                    sliderStart = 1;
-                    sliderEnd = step;
-                } else {
-                    sliderEnd = sliderEnd - step;
-                    sliderStart = sliderStart - step;
-                }
-                showImages(sliderStart, sliderEnd, imgStickers);
-                leftSpanNumb.textContent = sliderStart;
-                rightSpanNumb.textContent = sliderEnd;
-                for(let dot of sliderDots) {
-                    if(parseInt(dot.getAttribute('data-value')) < currentDot || parseInt(dot.getAttribute('data-value')) > currentDot) {
-                        if(dot.classList.contains('current')){
-                            dot.classList.remove('current');
-                        }
-                    } else {
-                        dot.classList.add('current');
-                    }
-                }
-            })
-        }
-
-    });
-
-
-
-    </script>";
-
-    $content .= '<script>
-    jQuery("#item-image-section").resizable_split({
-        handleSelector: "#item-splitter",
-        resizeHeight: false
-    });
-    </script>';
-
     $content .= "<style>
     #primary-full-width { padding: unset!important;}
     .highlight { color: #0a72cc;outline: 0.8px solid;}
     metadata {
         display: none;
-    }
-    #mark-active {
-        width: 60vw;
     }
     </style>";
 
