1e64a54f18860f1972af504c7f5904b74b6b54aa
Vejn <jsalesev@gmail.com>
2023-04-26 11:34:47 +0200

    Perf: comment out team shortcode

--- public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/team.php
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/team.php
@@ -10,31 +10,31 @@ include($_SERVER["DOCUMENT_ROOT"].'/wp-load.php');
 
 function _TCT_get_team( $atts ) {
 
+    return 0;
+    // $url = TP_API_HOST."/tp-api/teams?ShortName=".$_GET['team'];
+    // $requestType = "GET";
 
-    $url = TP_API_HOST."/tp-api/teams?ShortName=".$_GET['team'];
-    $requestType = "GET";
+    // // Execude http request
+    // include dirname(__FILE__)."/../custom_scripts/send_api_request.php";
 
-    // Execude http request
-    include dirname(__FILE__)."/../custom_scripts/send_api_request.php";
+    // // Save image data
+    // $team = json_decode($result, true);
 
-    // Save image data
-    $team = json_decode($result, true);
+    // // Build Front page content
+    // $content = "";
+    // if (sizeof($team) != 1) {
+    //     echo "";
+    // }
+    // else {
+    //     $team = $team[0];
+    //     foreach ($team['Users'] as $user) {
+    //         $userData = get_user_by( 'id', $user['WP_UserId'] );
+    //         var_dump($userData->user_nicename);
+    //     }
+    //     $content .= "test";
 
-    // Build Front page content
-    $content = "";
-    if (sizeof($team) != 1) {
-        echo "";
-    }
-    else {
-        $team = $team[0];
-        foreach ($team['Users'] as $user) {
-            $userData = get_user_by( 'id', $user['WP_UserId'] );
-            var_dump($userData->user_nicename);
-        }
-        $content .= "test";
-
-        echo $content;
-    }
+    //     echo $content;
+    // }
 }
 add_shortcode( 'get_team',  '_TCT_get_team' );
 ?>
