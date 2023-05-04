a16d905888de9a58be24e94f3af13bb2afca3aca
trenc <github@tommyschmucker.de>
2023-05-04 15:48:10 +0200

    refactor: rewrite teams admin page

--- public_html/wp-content/themes/transcribathon/admin/inc/custom_admin_pages/teams-admin-page.js
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_admin_pages/teams-admin-page.js
@@ -2,9 +2,14 @@ document.addEventListener('alpine:init', () => {
 
 	Alpine.data('manage_teams', () => ({
 
+		toast: false,
+		toastType: 'success',
+		toastMessage: '',
 		teams: [],
 		wpUsers: JSON.parse(ALL_USERS),
 		selectedTeam: {},
+		searchTerm: '',
+		teamMemberSearch: [],
 		scrollElement: document.querySelector('#team-mangement'),
 
 		async init() {
@@ -15,12 +20,58 @@ document.addEventListener('alpine:init', () => {
 
 			if (!teamData.success) {
 
-				alert(teamData.error);
+				this.openToast('error', teamData.error);
 				return;
 
 			}
 
 			this.teams = teamData.data;
+
+		},
+
+		async addTeamMember (wpUserId, nicename) {
+
+			const userData = await (await fetch(THEME_URI + '/api-request.php/users?limit=1&page=1&orderBy=UserId&orderDir=asc&WP_UserId=' + wpUserId)).json();
+
+			if (!userData.success) {
+
+				this.openToast('error', 'Could not add user to team.')
+				return;
+
+			}
+
+			const user = {
+				UserId: userData.data[0].UserId,
+				WP_UserId: wpUserId,
+				nicename: nicename
+			}
+
+			if (this.selectedTeam.Users.find(selectedUser => selectedUser.UserId === user.UserId)) {
+
+				this.searchTerm = '';
+				this.teamMemberSearch = [];
+				return;
+
+			}
+
+			this.selectedTeam.Users.push(user);
+
+			this.searchTerm = '';
+			this.teamMemberSearch = [];
+
+		},
+
+		searchTeamMember () {
+
+			if (this.searchTerm.length < 3) {
+
+				return;
+
+			}
+
+			this.teamMemberSearch = this.wpUsers.filter(
+				wpUser => wpUser.user_nicename.toLowerCase().includes(this.searchTerm.toLowerCase())
+			);
 
 		},
 
@@ -32,7 +83,6 @@ document.addEventListener('alpine:init', () => {
 			});
 			this.scrollElement.scrollIntoView({ behavior: 'smooth' });
 
-			console.log(this.selectedTeam);
     },
 
     removeTeamMember(userId) {
@@ -41,11 +91,120 @@ document.addEventListener('alpine:init', () => {
 
     },
 
-    saveTeam(teamId) {
+    async removeTeam(teamId) {
 
-			console.log(teamId);
+			const remove = await (await fetch(THEME_URI + '/api-request.php/teams/' + teamId, {
+				method: 'DELETE'
+			})).json();
+
+			if (!remove.success) {
+
+				this.openToast('error', 'Could not remove team.');
+				return;
+
+			}
+
+			this.teams = this.teams.filter(team => team.TeamId !== teamId);
+			this.openToast('success', 'Team removed.');
 
     },
+
+    saveTeam(teamId = null) {
+
+			if (this.selectedTeam.Name === '') {
+
+				return;
+
+			}
+
+			if (!teamId) {
+
+				this.createTeam();
+				return;
+
+			}
+
+			this.updateTeam(teamId);
+
+    },
+
+		async updateTeam(teamId) {
+
+			const userIdArray = [];
+			for (const user of this.selectedTeam.Users) {
+
+				userIdArray.push(user.UserId);
+
+			}
+
+			const payload = {
+				Name: this.selectedTeam.Name,
+				ShortName: this.selectedTeam.ShortName,
+				Description: this.selectedTeam.Description,
+				UserIds: [...userIdArray]
+			};
+
+			if (this.selectedTeam.Code !== '') {
+
+				payload.Code = this.selectedTeam.Code;
+
+			}
+
+			const updated = await (await fetch(THEME_URI + '/api-request.php/teams/' + teamId, {
+				method: 'PUT',
+				body: JSON.stringify(payload)
+			})).json();
+
+			if (updated.success) {
+
+				this.teams.map(team => team.TeamId === teamId ? updated.data : team);
+				this.resetForm();
+				this.openToast('success', 'Team updated.');
+
+			} else {
+
+				this.openToast('error', 'Team could not updated.');
+
+			}
+
+		},
+
+		async createTeam() {
+
+			const userIdArray = [];
+			for (const user of this.selectedTeam.Users) {
+
+				userIdArray.push(user.UserId);
+
+			}
+
+			const payload = {
+				Name: this.selectedTeam.Name,
+				ShortName: this.selectedTeam.ShortName,
+				Description: this.selectedTeam.Description,
+				UserIds: [...userIdArray],
+				Code: this.selectedTeam.Code
+			};
+
+			const created = await (await fetch(THEME_URI + '/api-request.php/teams', {
+				method: 'POST',
+				body: JSON.stringify(payload)
+			})).json();
+
+			if (created.success) {
+
+				this.selectedTeam = { ...created.data };
+				this.teams.push(this.selectedTeam);
+				this.resetForm();
+				this.openToast('success', 'Team created.');
+
+			} else {
+
+				this.openToast('error', 'Team could not created.');
+
+			}
+
+		},
 
     resetForm() {
 
@@ -54,39 +213,46 @@ document.addEventListener('alpine:init', () => {
 				Name: '',
 				ShortName: '',
 				TeamId: null,
+				Code: '',
 				Users: []
     	};
 
-    }
+    },
+
+    openToast(type, message) {
+
+    	if (this.toast) {
+
+    		return;
+
+    	}
+
+    	this.toast = true;
+    	this.toastType = type;
+    	this.toastMessage = message;
+
+			if (typeof timer !== 'undefined') {
+
+				clearTimeout(timer);
+
+			}
+
+			timer = setTimeout(() => {
+
+				this.toast = false;
+				this.toastMessage = '';
+
+      }, 3000);
+
+    },
+
+    closeToast() {
+
+    	this.toast = false;
+    	this.toastMessage = '';
+
+    },
 
 	}));
 
 });
-
-function addTeam() {
-    const teamName = document.getElementById('teamName').value;
-    const teamMembers = document.getElementById('teamMembers').value;
-
-    if (teamName === '') {
-        alert('Team name is required.');
-        return;
-    }
-
-    teams.push({
-        id: Date.now(),
-        name: teamName,
-        members: teamMembers
-    });
-
-    resetForm();
-    updateTeamList();
-}
-
-// Edit team
-
-// Delete team
-function deleteTeam(teamId) {
-    teams = teams.filter(t => t.id !== teamId);
-    updateTeamList();
-}
-
--- public_html/wp-content/themes/transcribathon/admin/inc/custom_admin_pages/teams-admin-page.php
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_admin_pages/teams-admin-page.php
@@ -7,76 +7,404 @@ function _TCT_teams_admin_page($atts)
 	$users = json_encode(
 		get_users([
 			'fields' => ['user_nicename', 'id'],
-			'role'   => 'subscriber',
+			// 'role'   => 'subscriber',
 			'exclude' => [1]
 		])
 	);
 
+	$tailwindLabelClasses = <<<TW1
+		before:content[' ']
+		after:content[' ']
+		pointer-events-none
+		absolute
+		left-0
+		-top-1.5
+		flex
+		h-full
+		w-full
+		select-none
+		text-[11px]
+		font-normal
+		leading-tight
+		text-blue-gray-400
+		transition-all
+		before:pointer-events-none
+		before:mt-[6.5px]
+		before:mr-1
+		before:box-border
+		before:block
+		before:h-1.5
+		before:w-2.5
+		before:rounded-tl-md
+		before:border-t
+		before:border-l
+		before:border-blue-gray-200
+		before:transition-all
+		after:pointer-events-none
+		after:mt-[6.5px]
+		after:ml-1
+		after:box-border
+		after:block
+		after:h-1.5
+		after:w-2.5
+		after:flex-grow
+		after:rounded-tr-md
+		after:border-t
+		after:border-r
+		after:border-blue-gray-200
+		after:transition-all
+		peer-placeholder-shown:text-sm
+		peer-placeholder-shown:leading-[3.75]
+		peer-placeholder-shown:text-blue-gray-500
+		peer-placeholder-shown:before:border-transparent
+		peer-placeholder-shown:after:border-transparent
+		peer-focus:text-[11px]
+		peer-focus:leading-tight
+		peer-focus:text-blue-500
+		peer-focus:before:border-t-2
+		peer-focus:before:border-l-2
+		peer-focus:before:border-blue-500
+		peer-focus:after:border-t-2
+		peer-focus:after:border-r-2
+		peer-focus:after:border-blue-500
+		peer-disabled:text-transparent
+		peer-disabled:before:border-transparent
+		peer-disabled:after:border-transparent
+		peer-disabled:peer-placeholder-shown:text-blue-gray-500
+TW1;
+
+	$tailwindInputClasses = <<<TW2
+		peer
+		h-full
+		w-full
+		rounded-[7px]
+		border
+		border-blue-gray-200
+		border-t-transparent
+		bg-transparent
+		px-3
+		py-2.5
+		text-sm
+		font-normal
+		text-blue-gray-700
+		outline
+		outline-0
+		transition-all
+		placeholder-shown:border
+		placeholder-shown:border-blue-gray-200
+		placeholder-shown:border-t-blue-gray-200
+		focus:border-2
+		focus:border-blue-500
+		focus:border-t-transparent
+		focus:outline-0
+		disabled:border-0
+		disabled:bg-blue-gray-50
+TW2;
+
+	$tailwindInputWrapperClasses = <<<TW3
+		relative
+		mb-4
+		h-11
+		w-full
+		min-w-[200px]
+TW3;
+
 	$html = <<<HTML
 <div class="container mx-auto mt-8" x-data="manage_teams" id="team-mangement">
     <h1 class="text-2xl mb-4">Team Management</h1>
+
     <div class="bg-white p-6 rounded shadow-md">
-        <div class="mb-4">
-            <label for="teamName" class="block mb-2">Team Name</label>
-            <input type="text" id="teamName" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter team name" x-model="selectedTeam.Name" />
-        </div>
-        <div class="mb-4">
-            <label for="teamName" class="block mb-2">Short Name</label>
-            <input type="text" id="teamName" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter team name" x-model="selectedTeam.ShortName" />
-        </div>
-        <div class="mb-4">
-            <label for="teamName" class="block mb-2">Description</label>
-            <input type="text" id="teamName" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter team name" x-model="selectedTeam.Description" />
-        </div>
-        <div class="mb-4">
-            <label for="teamMembers" class="block mb-2">Team Members</label>
-            <input type="text" id="teamMember" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter name of team member" />
-            <div class="min-h-[5rem] w-full flex gap-1 mt-4 px-4 py-2 border border-gray-300 rounded">
-							<template x-for="user in selectedTeam.Users">
-								<div class="w-fit h-fit bg-slate-200 rounded flex items-center">
-                	<div class="p-2" x-text="user.nicename"></div>
-                	<div @click="removeTeamMember(user.UserId)" class="p-2 select-none rounded-r-md cursor-pointer hover:bg-magma-orange-clear">
-                    <svg width="10" height="10" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12.5745 1L1 12.5745" stroke="#ef4444" stroke-width="2" stroke-linecap="round"/>
-                      <path d="M1.00024 1L12.5747 12.5745" stroke="#ef4444" stroke-width="2" stroke-linecap="round"/>
-                    </svg>
-                	</div>
-            		</div>
-							</template>
+
+			<div class="{$tailwindInputWrapperClasses}">
+				<input
+					class="{$tailwindInputClasses}"
+					x-model="selectedTeam.Name"
+					placeholder=" "
+				/>
+				<label
+					class="{$tailwindLabelClasses}"
+				>Team Name</label>
+			</div>
+
+			<div class="{$tailwindInputWrapperClasses}">
+				<input
+					class="{$tailwindInputClasses}"
+					x-model="selectedTeam.ShortName"
+					placeholder=" "
+				/>
+				<label
+					class="{$tailwindLabelClasses}"
+				>Team Short Name</label>
+			</div>
+
+			<div class="{$tailwindInputWrapperClasses}">
+				<input
+					class="{$tailwindInputClasses}"
+					x-model="selectedTeam.Description"
+					placeholder=" "
+				/>
+				<label
+					class="{$tailwindLabelClasses}"
+				>Team Description</label>
+			</div>
+
+			<div class="{$tailwindInputWrapperClasses}">
+				<input
+					class="{$tailwindInputClasses}"
+					x-model="selectedTeam.Code"
+					placeholder=" "
+				/>
+				<label
+					class="{$tailwindLabelClasses}"
+				>Team Code (only visible when inserting)</label>
+			</div>
+
+			<div class="{$tailwindInputWrapperClasses}">
+				<input
+					class="{$tailwindInputClasses}"
+					x-model="searchTerm"
+					@keyup="searchTeamMember()"
+					placeholder=" "
+				/>
+				<label
+					class="{$tailwindLabelClasses}"
+				>Search and add Team Member</label>
+			</div>
+
+			<div class="mb-4 min-h-11">
+				<ul class="
+					flex
+					flex-row
+					flex-wrap
+					content-start
+					items-center
+					gap-2
+					mt-3
+				">
+					<template x-for="member in teamMemberSearch">
+						<li class="">
+							<button
+								class="
+									center
+									relative
+									inline-block
+									select-none
+									whitespace-nowrap
+									rounded-lg
+									bg-green-500
+									py-2
+									px-3.5
+									align-baseline
+									text-xs
+									font-bold
+									leading-none
+									text-white
+								"
+								x-text="member.user_nicename"
+								@click="addTeamMember(member.id, member.user_nicename);"
+							></button>
+						</li>
+					</template>
+				</ul>
+			</div>
+      <div class="mb-4">
+        <div class="
+					{$tailwindInputClasses}
+					min-h-[5rem]
+					w-full
+					flex
+					flex-row
+					flex-wrap
+					content-start
+					items-center
+					gap-1
+				">
+					<template x-for="user in selectedTeam.Users">
+						<div class="
+							center
+							relative
+							inline-block
+							select-none
+							whitespace-nowrap
+							rounded-lg
+							bg-teal-500
+							py-2
+							px-3.5
+							align-baseline
+							text-xs
+							font-bold
+							leading-none
+							text-white
+						">
+							<div class="mr-5 mt-px" x-text="user.nicename"></div>
+							<div class="
+								absolute
+								top-1
+								right-1
+								mx-px
+								mt-[0.5px]
+								w-max
+								rounded-md
+								bg-red-500
+								transition-colors
+								hover:bg-red-600
+							">
+								<div
+									role="button"
+									class="h-5 w-5 p-1"
+									@click="removeTeamMember(user.UserId)"
+								>
+      						<svg
+        						xmlns="http://www.w3.org/2000/svg"
+        						fill="none"
+        						viewBox="0 0 24 24"
+        						stroke="currentColor"
+        						stroke-width="3"
+      						>
+        						<path
+          						stroke-linecap="round"
+          						stroke-linejoin="round"
+          						d="M6 18L18 6M6 6l12 12"
+        						></path>
+      						</svg>
+    						</div>
+  						</div>
 						</div>
-        </div>
-        <div class="flex justify-end">
-            <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded" @click="saveTeam(selectedTeam.TeamId)">Save</button>
-            <button type="button" class="ml-2 px-4 py-2 bg-gray-500 text-white rounded" @click="resetForm">Cancel</button>
-        </div>
-    </div,>
-    <table class="mt-8 w-full table-auto bg-white rounded shadow-md">
-        <thead class="bg-gray-200">
-            <tr>
-                <th class="px-4 py-2">Team Name</th>
-                <th class="px-4 py-2">Description</th>
-                <th class="px-4 py-2 w-40">Actions</th>
-            </tr>
+					</template>
+				</div>
+      </div>
+
+      <div class="flex justify-end">
+      	<button
+					type="submit"
+					class="
+						mr-3
+						rounded-lg
+						bg-blue-500
+						py-3
+						px-6
+						text-xs
+						font-bold
+						uppercase
+						text-white
+						shadow-md
+						shadow-blue-500/20
+						transition-all
+						hover:shadow-lg
+						hover:shadow-blue-500/40
+						active:opacity-[0.85]
+						active:shadow-none
+					"
+					@click="saveTeam(selectedTeam.TeamId)"
+				>Save</button>
+        <button
+					type="button"
+					class="
+						rounded-lg
+						border
+						border-blue-500
+						py-3
+						px-6
+						text-xs
+						font-bold
+						uppercase
+						text-blue-500
+						transition-all
+						hover:opacity-75
+						active:opacity-[0.85]
+					"
+					@click="resetForm"
+				>Cancel</button>
+      </div>
+    </div>
+
+    <table class="
+			w-full
+			mt-4
+			text-sm
+			text-left
+			text-gray-500
+		">
+  		<thead class="
+				text-xs
+				text-gray-700
+				uppercase
+				bg-gray-50
+			">
+        <tr>
+            <th scope="col" class="px-6 py-3">Team Name</th>
+            <th scope="col" class="px-6 py-3">Description</th>
+            <th scope="col" class="px-6 py-3">Actions</th>
+        </tr>
         </thead>
         <tbody>
-					<template x-for="team in teams" :key="team.TeamId">
-						<tr>
-							<td class="px-4 py-2 border-y-2 border-gray-200" x-text="team.Name"></td>
-							<td class="px-4 py-2 border-y-2 border-gray-200" x-text="team.Description"></td>
-							<td class="px-4 py-2 border-y-2 border-gray-200">
+					<template x-for="(team, index) in teams" :key="team.TeamId">
+						<tr
+							class="border-b"
+							:class="index % 2 === 0 ? 'bg-whight' : 'bg-gray-50'"
+						>
+							<th cope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap" x-text="team.Name"></td>
+							<td class="px-6 py-4" x-text="team.Description"></td>
+							<td class="flex px-6 py-4">
 								<button
-                    class="px-2 py-1 bg-blue-500 text-white rounded"
-                    @click="loadTeam(team.TeamId)"
-                >Edit</button>
+									class="
+										mr-3
+										rounded-lg
+										bg-blue-500
+										py-2
+										px-4
+										text-xs
+										font-bold
+										uppercase
+										text-white
+										shadow-md
+										shadow-blue-500/20
+										transition-all
+										hover:shadow-lg
+										hover:shadow-blue-500/40
+										active:opacity-[0.85]
+										active:shadow-none
+									"
+                  @click="loadTeam(team.TeamId)"
+								>Edit</button>
                 <button
-                    class="ml-2 px-2 py-1 bg-red-500 text-white rounded"
-                    @click="deleteTeam(team.id)"
+									class="
+										mr-3
+										rounded-lg
+										bg-red-500
+										py-2
+										px-4
+										text-xs
+										font-bold
+										uppercase
+										text-white
+										shadow-md
+										shadow-red-500/20
+										transition-all
+										hover:shadow-lg
+										hover:shadow-red-500/40
+										active:opacity-[0.85]
+										active:shadow-none
+									"
+                  @click="removeTeam(team.TeamId)"
                 >Delete</button>
 							</td>
 						</tr>
 					</template>
 					</tbody>
     </table>
+  	<button
+			type="button"
+			@click="closeToast()"
+			x-show="toast"
+			x-transition.duration.300ms
+			class="
+				fixed top-16 right-4 z-50 rounded-lg p-4 text-base leading-5 font-bold text-white opacity-100 transition
+				"
+			:class="toastType === 'success' ? 'bg-green-500 hover:bg-green-600' : 'bg-red-500 hover:bg-red-600'"
+		>
+      <p x-text="toastMessage"></p>
+		</button>
 </div>
 HTML;
 
@@ -107,10 +435,12 @@ function teams_menu()
 function load_teams_scripts()
 {
 	// using the playground tailwindcss for now, no compiling but heavier load
-	wp_register_script('add_tailwindcss', get_stylesheet_directory_uri(). '/admin/inc/custom_js/tailwindcss.min.js', [], '3.3.1', false);
+	// wp_register_script('add_tailwindcss', get_stylesheet_directory_uri(). '/admin/inc/custom_js/tailwindcss.min.js', [], '3.3.1', false);
+	// wp_enqueue_script('add_tailwindcss');
+	wp_register_style('add_css', get_stylesheet_directory_uri(). '/admin/inc/custom_admin_pages/teams-admin-page-css.min.css', [], '3.3.2', false);
 	wp_register_script('add_alpinejs', get_stylesheet_directory_uri(). '/admin/inc/custom_js/alpinejs.min.js', [], '3.12.0', false);
 	wp_register_script('add_team_script', get_stylesheet_directory_uri(). '/admin/inc/custom_admin_pages/teams-admin-page.js', [], '0.1.0', true);
-	wp_enqueue_script('add_tailwindcss');
+	wp_enqueue_style('add_css');
 	wp_enqueue_script('add_alpinejs');
 	wp_enqueue_script('add_team_script');
 	add_filter('script_loader_tag', 'defer_alpinejs', 10, 3);
