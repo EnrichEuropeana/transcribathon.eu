c581ab4ba212381a7a3c34978d3d748961007730
Vejn <jsalesev@gmail.com>
2023-05-23 17:25:45 +0200

    Feat: single team update

--- public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/team.php
+++ public_html/wp-content/themes/transcribathon/admin/inc/custom_shortcodes/team.php
@@ -82,23 +82,101 @@ function _TCT_get_team( $atts ) {
         
         if(!empty($teamData)) {
 
-            $content .= '<div class="container mx-auto">';
+            // Bubbles css
+            $twStatsBubblesClasses = <<<TW1
+                w-20
+                md:w-26
+                lg:w-40
+                h-20
+                md:h-26
+                lg:h-40
+                border-4
+                border-solid
+                rounded-full
+                border-gray-400
+                text-center
+                flex
+                flex-col
+                justify-center
+            TW1;
+
+            $twStatsNumbersClasses = <<<TW2
+                m-0
+                text-sm
+                md:text-base
+                lg:text-xl
+                font-bold
+                text-gray-400
+            TW2;
+
+            $twStatsLabelsClasses = <<<TW3
+                m-0
+                text-[11px]
+                lg:text-sm
+                text-gray-400
+                uppercase
+            TW3;
+
+            $content .= '<div class="container mx-auto max-w-[80%]">';
                 $content .= '<h2 class="theme-color text-xl font-bold mb-8">' . $teamData[0]['Name'] . ' (' . $teamData[0]['ShortName'] . ')</h2>';
 
                 if(!empty($teamData[0]['Description'])) {
                     $content .= '<h3 class="theme-color text-base font-bold mb-4"> Description: </h3>';
                     $content .= '<p class="my-0 text-sm">' . $teamData[0]['Description'] . '</p>';
                 }
+                $content .= '<hr class=" bg-gray-500 my-6 ">';
 
-                $content .= '<div>';
-                    $content .= '<div class="">';
+                // Team statistics bubbles
+            
+                $content .= '<div
+                    class="
+                        grid
+                        grid-cols-1
+                        sm:grid-cols-3
+                        gap-y-2
+                        md:gap-x-5
+                        place-items-center
+                        my-6
+                    ">';
+
+                    $content .= '<div class=" ' . $twStatsBubblesClasses . ' ">';
+                        $content .= '<p class=" ' . $twStatsNumbersClasses . ' ">' . number_format_i18n(count($teamData[0]['Users'])) . '</p>';
+                        $content .= '<p class=" ' . $twStatsLabelsClasses . ' "> Members </p>';
+                    $content .= '</div>';
+
+                    $content .= '<div class=" ' . $twStatsBubblesClasses . ' ">';
+                        $content .= '<p class=" ' . $twStatsNumbersClasses . ' ">' . number_format_i18n('11001') . '</p>';
+                        $content .= '<p class=" ' . $twStatsLabelsClasses . ' "> Characters </p>';
+                    $content .= '</div>';
+
+                    $content .= '<div class=" ' . $twStatsBubblesClasses . ' ">';
+                        $content .= '<p class=" ' . $twStatsNumbersClasses . ' ">' . number_format_i18n('512') . '</p>';
+                        $content .= '<p class=" ' . $twStatsLabelsClasses . ' "> Documents </p>';
+                    $content .= '</div>';
+                
+                $content .= '</div>';
+
+                
+
+                $content .= '<div class="lg:flex">';
+                    $content .= '<div
+                        class="
+                            flex-initial
+                            w-full
+                            lg:w-1/2
+                        ">';
                         $content .= '<h3 class="theme-color text-base font-bold my-8"> Members </h3>';
                         foreach($teamData[0]['Users'] as $member) {
                             $content .= '<p class="text-sm my-0">' . $member['UserId'] . '</p>';
                         }
                     $content .= '</div>';
     
-                    $content .= '<div class="">';
+                    $content .= '<div
+                        class="
+                            flex-initial
+                            w-full
+                            lg:w-1/2
+                        ">';
                         $content .= '<h3 class="theme-color text-base font-bold my-8"> Campaigns </h3>'; 
                         foreach($teamData[0]['Campaigns'] as $run) {
                             $content .= '<p class="text-sm my-1"><a href="' . get_europeana_url() . '/runs/' . str_replace(' ', '-',$run['Name']) . '" target="_blank">' . $run['Name'] . '</a></p>';
